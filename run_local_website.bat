18057d7657d63106f6205fc2a8786d24f5f417fb
Vaclav Elias <vaclavelias@gmail.com>
2023-04-08 18:49:06 +0100

    feat: new docfx introduced

---
+++ run_local_website.bat
@@ -0,0 +1,8 @@
+rem mkdir _site
+rem cd _site
+rem start "" http://localhost:8080/en/index.html
+rem ..\deps\docfx\docfx.exe serve
+
+rem stride-docs-next
+rem launch and open in http://localhost:8080/
+docfx en\docfx.json --serve
