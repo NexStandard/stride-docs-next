220613bd0abaf44c3bd583170d6f1ac84fc1074c
Vaclav Elias <vaclavelias@gmail.com>
2023-05-01 22:46:50 +0200

    feat: build bat files updated

--- run_local_website.bat
+++ run_local_website.bat
@@ -1,8 +1,8 @@
-rem mkdir _site
-rem cd _site
-rem start "" http://localhost:8080/en/index.html
-rem ..\deps\docfx\docfx.exe serve
+mkdir _site
+cd _site
+start "" http://localhost:8080/en/index.html
+docfx serve
 
 rem stride-docs-next
 rem launch and open in http://localhost:8080/
-docfx en\docfx.json --serve
+rem docfx en\docfx.json --serve
