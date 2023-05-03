f2ec0774e2e45626813db32d1035c387f8efe3d7
Vaclav Elias <vaclavelias@gmail.com>
2023-05-03 22:50:19 +0200

    feat: Language observer added

--- en/template/styles/main.js
+++ en/template/styles/main.js
@@ -1,3 +1,93 @@
+function waitForNavbarAndAddLanguageNavigation() {
+
+    // Select the target node to observe for changes
+    const targetNode = document.getElementById("navbar");
+
+    // If the target node is not found, display an error and exit
+    if (!targetNode) {
+        console.log('Navbar element not found');
+        return;
+    }
+
+    // Callback function to execute when the desired element is injected
+    const callback = function (mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                const navElement = document.querySelector('.navbar-nav');
+                if (navElement) {
+
+                    // Call your function to add the language navigation
+                    addLanguageNavigation();
+
+                    // Disconnect the observer once the element is found
+                    observer.disconnect();
+
+                    return;
+                }
+            }
+        }
+    };
+
+    // Create an observer instance with the callback function
+    const observer = new MutationObserver(callback);
+
+    // Options for the observer (which mutations to observe)
+    const config = { childList: true, subtree: true };
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+}
+
+function addLanguageNavigation() {
+
+    //const navElement = document.getElementById('navbar');
+    const navElement = document.querySelector('.navbar-nav');
+
+    if (!navElement) {
+        console.log('Navbar not found');
+        return;
+    }
+
+    //const languageList = document.createElement('ul');
+    //languageList.classList.add('navbar-nav');
+
+    const languageDropdown = document.createElement('li');
+    languageDropdown.classList.add('nav-item', 'dropdown');
+
+    const languageDropdownLink = document.createElement('a');
+    languageDropdownLink.classList.add('nav-link', 'dropdown-toggle');
+    languageDropdownLink.href = '#';
+    languageDropdownLink.role = 'button';
+    languageDropdownLink.setAttribute('data-bs-toggle', 'dropdown');
+    languageDropdownLink.setAttribute('aria-expanded', 'false');
+    languageDropdownLink.textContent = '🌐';
+
+    const dropdownMenu = document.createElement('ul');
+    dropdownMenu.classList.add('dropdown-menu');
+
+    const englishItem = document.createElement('li');
+    const englishLink = document.createElement('a');
+    englishLink.classList.add('dropdown-item');
+    englishLink.href = '#'; // Replace with the link to the English version of the page
+    englishLink.textContent = 'English';
+    englishItem.appendChild(englishLink);
+
+    const japaneseItem = document.createElement('li');
+    const japaneseLink = document.createElement('a');
+    japaneseLink.classList.add('dropdown-item');
+    japaneseLink.href = '#'; // Replace with the link to the Japanese version of the page
+    japaneseLink.textContent = 'Japanese';
+    japaneseItem.appendChild(japaneseLink);
+
+    dropdownMenu.appendChild(englishItem);
+    dropdownMenu.appendChild(japaneseItem);
+    languageDropdown.appendChild(languageDropdownLink);
+    languageDropdown.appendChild(dropdownMenu);
+    //languageList.appendChild(languageDropdown);
+    //navElement.appendChild(languageList);
+    navElement.appendChild(languageDropdown);
+}
+
 function addVersionNavigation() {
     const version = document.getElementById("toc");
     const selectHtml = `
@@ -10,16 +100,21 @@ function addVersionNavigation() {
 
 function loadVersions() {
     fetch('/versions.json')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error loading versions.json: ' + response.statusText);
+            }
+            return response.json();
+        })
         .then(data => {
             const selectElement = document.getElementById("stride-current-version");
             selectElement.innerHTML = '';
 
             // Add the "Latest" option
-            const latestOption = document.createElement('option');
-            latestOption.value = 'latest';
-            latestOption.textContent = 'Latest';
-            selectElement.appendChild(latestOption);
+            //const latestOption = document.createElement('option');
+            //latestOption.value = 'latest';
+            //latestOption.textContent = 'Latest';
+            //selectElement.appendChild(latestOption);
 
             data.versions.forEach(version => {
                 const url = version;
@@ -31,13 +126,15 @@ function loadVersions() {
 
             const urlSplits = window.location.pathname.split('/');
             let urlVersion = urlSplits[1];
-            //if (urlVersion === 'latest') {
-            //    urlVersion = data.versions[0];
-            //}
+            if (urlVersion === 'latest') {
+                urlVersion = data.versions[0];
+            }
 
             selectElement.value = urlVersion;
             selectElement.dispatchEvent(new Event('change'));
             redirectToCurrentDocVersion();
+        }).catch(error => {
+            console.log('Error loading or processing versions.json:', error);
         });
 }
 
@@ -74,6 +171,9 @@ function redirectToCurrentDocVersion() {
 
 
 function start() {
+
+    // Call the function to start waiting for the navbar element
+    waitForNavbarAndAddLanguageNavigation();
 
     addVersionNavigation()
     loadVersions();
