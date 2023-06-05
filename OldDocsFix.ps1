268aa118300e6afee3edfea77206f0def07fbafd
Vaclav Elias <vaclavelias@gmail.com>
2023-06-05 21:57:00 +0100

    fix: Update script to use relative urls

--- OldDocsFix.ps1
+++ OldDocsFix.ps1
@@ -1,14 +1,15 @@
-$path = 'd:\Projects\GitHub\stride-docs-live-fix\' # specify your directory path
+# specify your directory path
+$path = 'd:\Projects\GitHub\stride-docs-live-fix-2\'
 
 $replacements = @{
-    '//stride3d.net/css/site.css' = '/css/site.css';
-    '//xenko.com/css/site.css' = '/css/site.css';
-    '//stride3d.net/scripts/site.doc.js' = '/scripts/site.doc.js';
-    '//xenko.com/scripts/site.doc.js' = '/scripts/site.doc.js';
-    '//stride3d.net/favicon.png' = '/favicon.png';
-    '//xenko.com/favicon.png' = '/favicon.png';
-    '//stride3d.net/scripts/theme.js' = '/scripts/theme.js';
-    '//xenko.com/scripts/theme.js' = '/scripts/theme.js';
+    '//stride3d.net/css/site.css' = 'css/site.css';
+    '//xenko.com/css/site.css' = 'css/site.css';
+    '//stride3d.net/scripts/site.doc.js' = 'scripts/site.doc.js';
+    '//xenko.com/scripts/site.doc.js' = 'scripts/site.doc.js';
+    '//stride3d.net/favicon.png' = 'favicon.png';
+    '//xenko.com/favicon.png' = 'favicon.png';
+    '//stride3d.net/scripts/theme.js' = 'scripts/theme.js';
+    '//xenko.com/scripts/theme.js' = 'scripts/theme.js';
 }
 
 $files = Get-ChildItem -Path $path -Filter *.html -Recurse
@@ -19,10 +20,20 @@ foreach ($file in $files) {
     # if ($i -ge 100) { break } # Exit loop after 100 items
 
     $i++
-    $content = Get-Content $file.FullName -Encoding UTF8
-    $replacements.Keys | ForEach-Object {
-        $content = $content -replace $_, $replacements[$_]
+    $content = Get-Content $file.FullName -Encoding UTF8 -Raw
+
+   # Get content of 'docfx:rel' meta tag
+    $metaContent = ""
+    if ($content -match '<meta\s+property="docfx:rel"\s+content="([^"]*)">') {
+        if ($Matches.Count -gt 1) {
+            $metaContent = $Matches[1]
+        }
     }
+
+    $replacements.Keys | ForEach-Object {
+        $content = $content -replace $_, ($metaContent + $replacements[$_])
+    }
+
     Set-Content -Path $file.FullName -Value $content -Encoding UTF8
 
     # Update progress bar
